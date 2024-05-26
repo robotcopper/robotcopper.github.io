6fda9a71a9ca61f08393eb877cdd302ab0fd00b1
robotcopper <95591405+robotcopper@users.noreply.github.com>
2024-05-26 21:16:55 +0200

    Fix: scroll and sidebars use

--- config/_includes/extra/script.js
+++ config/_includes/extra/script.js
@@ -1,5 +1,7 @@
 $("#toggle2").click(function () {
-  $(".sidebar-wrap-2,.content-wrap, .archive").toggleClass("shift2");
+  $(".sidebar-wrap-2,.sidebar-wrap,.content-wrap, .archive").toggleClass("shift2");
 });
 
-
+$("#toggle").click(function () {
+  $(".sidebar-wrap-2").toggleClass("shiftbis");
+});
